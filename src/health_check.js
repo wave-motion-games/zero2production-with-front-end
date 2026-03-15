2e9080c484d55a41c69320e9f6c6db41d2eea016
wme <wave_motion_engine@proton.me>
2026-03-14 20:32:40 -0500

    removing dead files

--- src/health_check.js
+++
@@ -1,7 +0,0 @@
-export default async function healthCheck() {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/health_check`);
-    if (!response.ok) {
-        alert('failed to contact server. Some services may not be available');
-        return;
-    }
-}
